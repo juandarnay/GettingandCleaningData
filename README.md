329b3886130a5126d97daca24c1641174b09e210
CRizzo'code <rizzo92.caterina@gmail.com>
2020-12-03 22:57:32 +0100

    Final Commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# GettingandCleaningData
-Programming Assignment DS Specialization Course 
+# Getting and Cleaning Data Programming Assignment 
+***
+This repository was created for the "Getting and Cleaning Data course programming assignment. 
+
+The data used for this assignment can be downloaded from the following link: https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+The original data files are also stored in this repository in the folder "UCI HAR Dataset".
+
+The 'run_analysis.R' script, which does the following:
+
+1. Merges the training and the test sets from the original files to create one data set
+2. Extracts only the measurements on the mean and standard deviation for each measurement
+3. Uses descriptive activity names to name the activities in the data set
+4. Labels the data set with descriptive variable names. 
+5. Write the tidy summary in a txt file in the working directory. 
+
+The "HumanActivity_Tidy_Summary" is the product of the 'run_analysis.R' over the original data set. 
+
+The "CodeBook file contains general information about the original data set and a full description of the tidy data set "HumanActivity_Tidy_Summary". 
--- run_analysis.R
+++ run_analysis.R
@@ -1,8 +1,14 @@
+# Download and read the test and training sets from the txt files provided
 
+if(!file.exists("UCI HAR Dataset")){
+  dir.create("UCI HAR Dataset")
+}
 
-# Read the test and training sets from txt
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(fileUrl, destfile = "./UCI HAR Dataset.zip")
+unzip("UCI HAR Dataset.zip")
 
-path <- "UCI HAR Dataset"
+path <- "/UCI HAR Dataset"
 
 subj_train <- read.table(file.path(path, "train", "subject_train.txt" ))
 X_train <- read.table(file.path(path, "train", "X_train.txt" ))
@@ -12,47 +18,52 @@ subj_test <- read.table(file.path(path, "test", "subject_test.txt" ))
 X_test <- read.table(file.path(path, "test", "X_test.txt" ))
 Y_test <- read.table(file.path(path, "test", "Y_test.txt" ))
 
-train_dataset <- cbind(subj_train, Y_train, X_train)
-test_dataset <- cbind(subj_test, Y_test, X_test)
+train<- cbind(subj_train, Y_train, X_train)
+test<- cbind(subj_test, Y_test, X_test)
 
-all_dataset <- rbind(train_dataset, test_dataset)
+HumanActivity <- rbind(train, test)
 
 features <- read.table(file.path(path, "features.txt"), as.is = TRUE)
 activity_labels <- read.table(file.path(path, "activity_labels.txt"), as.is = TRUE)
 
-colnames(all_dataset) <- c("Subject", "Activity", features[ ,2])
+colnames(HumanActivity) <- c("Subject", "Activity", features[ ,2])
 
-all_dataset$Activity <- factor(all_dataset$Activity, activity_labels[,1], activity_labels[,2])
+HumanActivity$Activity <- factor(HumanActivity$Activity, activity_labels[,1], activity_labels[,2])
 
-##########################################################################################
-## Extracts only the measurements on the mean and standard deviation for each measurement.
+## Extracts only the measurements on the mean and standard deviation for each measurement
+## and create a new datatable with the selected variables, including the "Subject ID" and "Activity"
 
-column_extr <- grepl("mean|sdt", colnames(all_dataset) ) 
-all_subset <- all_dataset[, column_extr]
+columns_extr <- grepl("Subject|Activity|mean|sdt", colnames(HumanActivity) ) 
+HumanActivity <- HumanActivity[, columns_extr]
 
-names(all_dataset)
-all_dataset<- # get column names
-  har_Cols <- colnames(all_dataset)
+names(HumanActivity)
+HumanActivity_Cols <- colnames(HumanActivity)
 
-# remove special characters
-humanActivityCols<- gsub("\\-", " ", har_Cols)
-humanActivityCols <- gsub("BodyBody", "Body", har_Cols)
-# expand abbreviations and clean up names
+## Remove special characters and typos
 
-humanActivityCols <- gsub("^f", "frequencyDomain", humanActivityCols)
-humanActivityCols <- gsub("^t", "timeDomain", humanActivityCols)
-humanActivityCols <- gsub("Acc", "Accelerometer", humanActivityCols)
-humanActivityCols <- gsub("Gyro", "Gyroscope", humanActivityCols)
-humanActivityCols <- gsub("Mag", "Magnitude", humanActivityCols)
-humanActivityCols <- gsub("Freq", "Frequency", humanActivityCols)
-humanActivityCols <- gsub("mean", "Mean", humanActivityCols)
-humanActivityCols <- gsub("std", "StandardDeviation", humanActivityCols)
+HumanActivity_Cols <- gsub("\\-", " ", HumanActivity_Cols)
+HumanActivity_Cols <- gsub("BodyBody", "Body", HumanActivity_Cols)
+
+## Explicit some of the indicators in the variables' names
+
+HumanActivity_Cols <- gsub("^f", "frequencyDomain ", HumanActivity_Cols)
+HumanActivity_Cols <- gsub("^t", "timeDomain ", HumanActivity_Cols)
+HumanActivity_Cols <- gsub("Acc", "Accelerometer", HumanActivity_Cols)
+HumanActivity_Cols <- gsub("Gyro", "Gyroscope", HumanActivity_Cols)
+HumanActivity_Cols <- gsub("Mag", "Magnitude", HumanActivity_Cols)
+HumanActivity_Cols <- gsub("Freq", "Frequency", HumanActivity_Cols)
+HumanActivity_Cols <- gsub("mean()", "Mean", HumanActivity_Cols)
+HumanActivity_Cols <- gsub("std()", "StandardDeviation", HumanActivity_Cols)
+
+colnames(HumanActivity) <- HumanActivity_Cols
+
+## Create a second, independent tidy set with the average of each variable for each activity and each subject
+
+HumanActivity_Summary <- aggregate(. ~ Subject + Activity, data = HumanActivity, mean)
+
+## Save the tidy set as a text file 
+
+write.table(HumanActivity_Summary , "HumanActivity_Tidy_Summary.txt", row.names = FALSE)
 
 
-##############################################################################
-# Create a second, independent tidy set with the average of each variable for each activity and each subject
-
-humActivity_stats <- aggregate(. ~ Subject + Activity, data = all_dataset, mean)
-
-
-##########
+                                       
